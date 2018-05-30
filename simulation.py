13968385b0cf475da35c2f4280ec66270c229d94
Alla Polisskaya <apolisskaya2016@fau.edu>
2018-05-30 14:54:14 -0400

    simulation is now available as an executable, formatting is now possible for logging and readability purposes

---
+++ simulation.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    from Services import format_service as fs
+    fs.pretty_print("Welcome to the FAU I-SENSE Mobile Charging Simulator", 'purple')
